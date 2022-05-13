87d9d16eb8bbf15ac4ea845e9d4f415eeb0d4d3a
Luke Dowell <luke.dowell@focusedlabs.io>
2022-05-13 14:24:31 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { ScoreResultView } from './ScoreResultView'
+
+const App = () => {
+  const [did, setDid] = useState<string>('')
+  const [didInput, setDidInput] = useState<string>('')
+
+  const scorePassport = () => {
+    setDid(didInput)
+  }
+
+  return (
+    <div className="container mx-auto flex flex-wrap px-5 py-24 sm:flex-nowrap">
+      <div className="flex flex-col rounded-lg p-10 sm:mr-10 md:w-1/2 lg:w-1/2">
+        <div className="font-miriam-libre text-gray-050 mt-10 font-bold leading-relaxed">
+          <p className="text-6xl">
+            A Decentralized
+            <br />
+            Scorer Application
+          </p>
+        </div>
+        <div className="font-libre-franklin mt-10 text-xl">
+          Project dPopp is a passport for Web3 citizens - issued through a
+          collection of trusted sources that confirm your identity. Designed to
+          provide proof through your passport, dPopp verifies your Personhood
+          strengthening your sybil resistance. Decentralized on Ceramic Network.
+        </div>
+        <div className="mb-10 mt-10">
+          <input
+            type="did"
+            id="did"
+            name="did"
+            placeholder="Enter your dID"
+            onChange={(e) => setDidInput(e.target.value)}
+            className="w-full rounded border border-gray-300 bg-white py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+          />
+          <button
+            onClick={scorePassport}
+            className="mt-4 w-full rounded border-0 bg-indigo-400 py-2 px-6 text-lg text-white hover:bg-indigo-600 focus:outline-none"
+          >
+            <p className="text-base">Score My Passport</p>
+          </button>
+        </div>
+      </div>
+      <div className="mt-8 flex w-full flex-col bg-white md:ml-auto md:mt-0 md:w-1/2 md:py-8 lg:w-1/3">
+        {did && <ScoreResultView did={did} />}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/ScoreResultView.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import type { ModelTypeAliases } from '@glazed/types'
+import { usePublicRecord } from '@self.id/framework'
+import { useEffect } from 'react'
+
+type VerifiableCredential = {
+  '@context': string[]
+  type: string[]
+  credentialSubject: {
+    id: string
+    '@context': { [key: string]: string }[]
+    root?: string
+    address?: string
+    challenge?: string
+  }
+  issuer: string
+  issuanceDate: string
+  expirationDate: string
+  proof: {
+    type: string
+    proofPurpose: string
+    verificationMethod: string
+    created: string
+    jws: string
+  }
+}
+
+type CeramicStamp = {
+  provider: string
+  credential: string
+}
+
+type CeramicPassport = {
+  issuanceDate: string
+  expiryDate: string
+  stamps: CeramicStamp[]
+}
+
+export type ModelTypes = ModelTypeAliases<
+  {
+    Passport: CeramicPassport
+    VerifiableCredential: VerifiableCredential
+  },
+  {
+    Passport: 'Passport'
+    VerifiableCredential: 'VerifiableCredential'
+  },
+  {}
+>
+
+export type ScoreResultViewProps = {
+  did: string
+}
+
+export const ScoreResultView = ({ did }: ScoreResultViewProps): JSX.Element => {
+  const record = usePublicRecord<ModelTypes, 'Passport'>('Passport', did)
+
+  useEffect(() => {
+    console.log(record)
+  }, [record])
+
+  return (
+    <div className="border-2 p-10 text-center">
+      {record?.isLoading ? (
+        <div>
+          <p>LOADING</p>
+        </div>
+      ) : (
+        <div className={'flex flex-col'}>
+          <div className="mb-20 underline">Your Score</div>
+          <p className="text-2xl">
+            {((record?.content as CeramicPassport)?.stamps || []).length > 0
+                ? <span data-testid="passport-score--good">'GOOD 👍'</span>
+                : <span data-testid="passport-score--bad">'BAD 👎'</span>}
+          </p>
+        </div>
+      )}
+    </div>
+  )
+}
